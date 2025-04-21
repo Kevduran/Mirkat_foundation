4ee096f60875eedee7f4656be9b8021e66582115
KevinC911 <00157320@uca.edu.sv>
2025-04-21 16:04:00 -0600

    feat: Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Front from "./Front"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
 
   return (
     <>
-      <Front/>
+    <Routes>
+      {/*Añadir tus rutas aquí*/}
+      <Route path="/" element={<Front />} />
+    </Routes>
     </>
   )
 }
 
-export default App
+export default App;
